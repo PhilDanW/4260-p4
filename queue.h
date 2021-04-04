2c7bd00e28acdab43a86feffae2ddda240e9f67e
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-04 18:32:25 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,4 +1,7 @@
-  
+//Author: Philip Wright
+//Project 4 - Process Scheduling - CMPSCI4760
+//queue.h contains the structure format and function defintions for the queues
+
 #ifndef QUEUE_H_
 #define QUEUE_H_
 struct Queue{
@@ -12,5 +15,9 @@ struct Queue* createQueue(int capacity);
 int queueSize(struct Queue* queue);
 int isFull(struct Queue* queue);
 int isEmpty(struct Queue* queue);
+void enqueue(struct Queue* queue,int controlBlock);
+int dequeue(struct Queue* queue);
+int front(struct Queue* queue);
+int rear (struct Queue* queue);
 
 #endif
